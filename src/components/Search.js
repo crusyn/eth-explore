ef5978c35d508959e7e3bf93dc7fd12af3bf772e
crusyn <c.rusyn@gmail.com>
2019-03-01 17:33:57 -0500

    working with controlled components

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,34 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
 
-const Search = ({ search }) => {
-  return (
-    <div>
-      <TextField
-        placeholder="search for an ethereum address to show account and transaction details"
-        autoFocus={true}
-        fullWidth={true}
-        onChange={search}
-      />
-    </div>
-  );
-};
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    console.log(event);
+    console.log("Query Submitted: " + this.state.value);
+    event.preventDefault();
+  }
+
+  //working with controlled components and submitting: https://reactjs.org/docs/forms.html
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <TextField
+          placeholder="search for an ethereum address to show account and transaction details"
+          autoFocus={true}
+          fullWidth={true}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
 
 export default Search;
